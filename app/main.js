d4f1f2c51db6190d5db13bdcd0ec9a8f18d663ce
vegeta897 <vegeta897@gmail.com>
2015-01-16 12:36:34 -0500

    Upload test

--- app/main.js
+++ app/main.js
@@ -2,5 +2,6 @@
 Application.Controllers.controller('Main', function($scope,$timeout) {
 
     $scope.version = 0.001; $scope.versionName = 'Mark Attack';
+    console.log('Main controller initialized!');
     
 });
